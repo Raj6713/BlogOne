8668183c53bd85bd4ea05dba1b3f794b6fe89b3b
rkumar60 <rkumar60@slb.com>
2018-12-09 19:18:40 +0530

    .gitignore file moved one level up from App

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kyn@nts$gb60(ftll5hm+usi4(x^5c%5r%4-0%yqds8lhb6y$1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'rajwaa21.pythonanywhere.com']
 
 
 # Application definition
